4f120dd3ec4ed61b44d0c7e5cf3c0b5a45fba54a
Alexey Vlasenko <alex.vlasenko1991@gmail.com>
2018-06-30 19:59:59 +0300

    Win project set-up
    Changed some settings in project and files to make windows project alive

--- bin/base_p/start.lua
+++ bin/base_p/start.lua
@@ -35,4 +35,4 @@ UploadResourceGroup("TestGroup")
 -- В этот момент у виджетов слоя один раз вызывается метод AcceptMessage("Init")
 -- и начинают вызываться методы Draw() и Update() в каждом кадре.
 --
-Screen:pushLayer("TestLayer")
+Screen:pushLayer("MainLayer")
